7626b47e15d47c6776b49a0a0e88b5f89bcc4736
Slava Khripushin <slava.cpa@yandex.ru>
2026-01-05 17:10:45 +0700

    init

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+	Fancybox.bind('[data-fancybox]', {
+		dragToClose: false,
+		autoFocus: false,
+		placeFocusBack: false,
+		Thumbs: false,
+		Images: {
+			zoom: false
+		}
+	});
+});
