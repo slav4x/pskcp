f390e1cee4c3709cfc7f395542dc2cea3bf0dc12
Slava Khripushin <slava.cpa@yandex.ru>
2026-01-09 14:52:04 +0700

    feat: activity

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
-	Fancybox.bind('[data-fancybox]', {
-		dragToClose: false,
-		autoFocus: false,
-		placeFocusBack: false,
-		Thumbs: false,
-		Images: {
-			zoom: false
-		}
-	});
+  Fancybox.bind('[data-fancybox]', {
+    dragToClose: false,
+    autoFocus: false,
+    placeFocusBack: false,
+    Thumbs: false,
+    Images: {
+      zoom: false,
+    },
+  });
+
+  const activityCarousel = new Splide('.activity-carousel', {
+    perPage: 3,
+    arrows: false,
+    gap: 20,
+    speed: 600,
+    rewind: false,
+    drag: false,
+  }).mount();
 });
