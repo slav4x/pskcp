7cf5fc1c9386a06fb9a51d1d210457e79bb0649a
Slava Khripushin <slava.cpa@yandex.ru>
2026-01-20 16:23:42 +0700

    feat: upd forms

--- src/js/main.js
+++ src/js/main.js
@@ -123,6 +123,15 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 
+  function generateToken() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let token = '';
+    for (let i = 0; i < 30; i++) {
+      token += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return token;
+  }
+
   function setToken(form) {
     const token = generateToken();
     const hiddenInput = document.createElement('input');
